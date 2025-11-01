e349a50a7f5b55e6876bcb6914abb0b8086487cb
jjmmnd <rnastery1@gmail.com>
2025-11-02 00:38:17 +0900

    [feat] Initial Commit

---
+++ src/main/java/B2A4/demoday/DemodayApplication.java
@@ -0,0 +1,13 @@
+package B2A4.demoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/B2A4/demoday/DemodayApplicationTests.java
@@ -0,0 +1,13 @@
+package B2A4.demoday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
